03659a76f11bb87892ed9d1fa10a1702b33a5980
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2022-12-12 12:37:40 -0300

    CSS setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import "./reset.css";
 import "./App.css";
+
+import Exercises from "./components/exercises/Exercises.tsx";
+import Heading from "./components/heading/Heading.tsx";
+import Hero from "./components/hero/Hero.tsx";
+import Series from "./components/series/Series.tsx";
+import WorkProgram from "./components/workProgram/WorkProgram.tsx";
 
 function App() {
   return (
